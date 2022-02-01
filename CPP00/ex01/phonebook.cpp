ab735b5887972df486b515225a09efdf164a9770
Lyphmeno <hugolevipro@gmail.com>
2022-02-01 14:02:04 +0100

    slight correction on ex01

--- CPP00/ex01/phonebook.cpp
+++ CPP00/ex01/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: hlevi <hlevi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 15:14:54 by hlevi             #+#    #+#             */
-/*   Updated: 2022/01/31 17:28:34 by hlevi            ###   ########.fr       */
+/*   Updated: 2022/02/01 14:00:24 by hlevi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,30 @@ void	fill_book(contact *book)
 	std::cin >> book->phone;
 	std::cout << "Darkest secret : ";
 	std::cin >> book->secret;
+}
+
+void	search_Data(std::string input, contact *book)
+{
+	int	i;
+
+	std::cout << "What index are you interested in ?" << std::endl;
+	std::cin >> input;
+	std::istringstream(input) >> i;
+	if (i != 0)
+	{
+		if (book[i - 1].id == 1)
+		{
+			std::cout << "First name : " << book[i - 1].first_name << std::endl;
+			std::cout << "Last name : " << book[i - 1].last_name << std::endl;
+			std::cout << "Nickname : " << book[i - 1].nickname << std::endl;
+			std::cout << "Phone number : " << book[i - 1].phone << std::endl;
+			std::cout << "Darkest secret : " << book[i - 1].secret << std::endl;
+		}
+		else
+			std::cout << "This person doesn't exist !" << std::endl;
+	}
+	else
+		std::cout << "What are you trying to tell me..." << std::endl;
 }
 
 int	main()
@@ -68,24 +92,7 @@ int	main()
 				std::cout << std::right << std::setw(10) << trunc(book[i].nickname) << "|" << std::endl;
 				i++;
 			}
-			std::cout << "What index are you interested in ?" << std::endl;
-			std::cin >> input;
-			std::istringstream(input) >> i;
-			if (i != 0)
-			{
-				if (book[i - 1].id == 1)
-				{
-					std::cout << "First name : " << book[i - 1].first_name << std::endl;
-					std::cout << "Last name : " << book[i - 1].last_name << std::endl;
-					std::cout << "Nickname : " << book[i - 1].nickname << std::endl;
-					std::cout << "Phone number : " << book[i - 1].phone << std::endl;
-					std::cout << "Darkest secret : " << book[i - 1].secret << std::endl;
-				}
-				else
-					std::cout << "This person doesn't exist !" << std::endl;
-			}
-			else
-				std::cout << "What are you trying to tell me..." << std::endl;
+			search_Data(input, book);
 		}
 	}
 	std::cout << "I hope you'll remember them because I won't" << std::endl;

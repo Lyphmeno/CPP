5539781b121d081ea5801b16f46bb1b014db546a
hlevi <hlevi.integra@gmail.com>
2022-06-03 16:17:56 +0200

    ex02 done

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlevi <hlevi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 18:17:27 by hlevi             #+#    #+#             */
-/*   Updated: 2022/02/01 18:23:41 by hlevi            ###   ########.fr       */
+/*   Updated: 2022/06/03 16:16:44 by hlevi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@ int	main(void)
 	std::string	*stringPTR = &stringBase;
 	std::string	&stringREF = stringBase;
 
-	std::cout << "ADRESS:" << std::endl;
+	std::cout << "ADDRESS:" << std::endl;
 	std::cout << "Base string adress: " << &stringBase << std::endl;
 	std::cout << "Pointer string adress: " << stringPTR << std::endl;
 	std::cout << "Reference string adress: " << &stringREF << std::endl;
+	std::cout << std::endl;
 	std::cout << "STRING:" << std::endl;
 	std::cout << "Base string: " << stringBase << std::endl;
 	std::cout << "Pointer string: " << *stringPTR << std::endl;

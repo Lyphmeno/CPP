2d04ad2b178082135b42056d7b3710b25e32c6ac
hlevi <hlevi.integra@gmail.com>
2022-06-03 16:00:55 +0200

    ex01 done

--- CPP01/ex00/Zombie.cpp
+++ CPP01/ex00/Zombie.cpp
@@ -6,25 +6,17 @@
 /*   By: hlevi <hlevi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 16:30:43 by hlevi             #+#    #+#             */
-/*   Updated: 2022/02/01 17:24:04 by hlevi            ###   ########.fr       */
+/*   Updated: 2022/06/03 15:57:41 by hlevi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie(std::string name)
-{
-	this->name = name;
-}	
+Zombie::Zombie(std::string name) { this->name = name; }	
 
-Zombie::~Zombie(void)
-{
-	std::cout << "<" << this->name << " died>"
-	 << std::endl;
-}
+Zombie::~Zombie() { std::cout << "<" << this->name << " died>" << std::endl; }
 
-void	Zombie::announce(void)
+void	Zombie::announce()
 {
-	std::cout << "<" << this->name << ">  "
-	<< "BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << this->name << ":\tBraiiiiiiinnnzzzZ..." << std::endl;
 }
--- CPP01/ex00/main.cpp
+++ CPP01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlevi <hlevi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 16:25:36 by hlevi             #+#    #+#             */
-/*   Updated: 2022/02/01 17:23:14 by hlevi            ###   ########.fr       */
+/*   Updated: 2022/06/03 15:59:45 by hlevi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 int	main(void)
 {
 	// Allocation on stack with randomChump
-	randomChump("The witch");
+	randomChump("Ed");
 
 	// Allocation on the heap with new and then delete
-	Zombie *boomer = newZombie("The boomer");
-	boomer->announce();
-	delete boomer;
+	Zombie *Shaun = newZombie("Shaun");
+	Shaun->announce();
+	delete Shaun;
 	return (0);
 }

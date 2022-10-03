7b70cd69c4ae536084465ed7bc5349f63b16e314
Lyfmeno <hugolevipro@gmail.com>
2022-10-03 16:00:10 +0200

    just some info on main

--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -6,16 +6,19 @@
 /*   By: hlevi <hlevi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 13:11:48 by hlevi             #+#    #+#             */
-/*   Updated: 2022/10/03 13:19:14 by hlevi            ###   ########.fr       */
+/*   Updated: 2022/10/03 15:20:19 by hlevi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cstdlib>
 #include "Base.hpp"
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
 
 // I might want to use dyamic_cast since they can only b used with pointer and ref to classes !
+// srand((unsigned int)time(NULL));
+// rand() % 3
 
 int		main()
 {

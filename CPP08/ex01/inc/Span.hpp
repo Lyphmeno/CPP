cd8a7007dff7e857c2082f22b36737c2b348ba6c
Lyfmeno <hugolevipro@gmail.com>
2022-10-13 16:58:38 +0200

    Begining of ex01

---
+++ CPP08/ex01/inc/Span.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlevi <hlevi@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/13 16:50:19 by hlevi             #+#    #+#             */
+/*   Updated: 2022/10/13 16:52:52 by hlevi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#define	LOG	0
+
+class Span
+{
+private:
+public:
+	// Coplien //
+	Span();
+	Span(const Span &cpy);
+	~Span();
+	Span &operator=(const Span &rhs);
+	// Assignation contructors //
+	// Operators //
+	// Getters //
+	// Setters //
+	// Methods //
+	// Exceptions //
+};

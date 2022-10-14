acd80d664466bfb507b456506841ffab1627f4de
Lyfmeno <hugolevipro@gmail.com>
2022-10-14 11:30:12 +0200

    Added all files needed and still teasting WebHook

---
+++ CPP08/ex02/inc/MutantStack.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class MutantStack
+{
+private:
+public:
+	// Coplien //
+	MutantStack();
+	MutantStack(const MutantStack &cpy);
+	~MutantStack();
+	MutantStack &operator=(const MutantStack &rhs);
+	// Assignation contructors //
+	// Operators //
+	// Getters //
+	// Setters //
+	// Methods //
+	// Exceptions //
+};
